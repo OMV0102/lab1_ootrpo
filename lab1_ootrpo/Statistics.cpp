a5724ebead57cdcee8a779f81146bf2e6f4e601f
omv0102 <omv98@outlook.com>
2021-01-07 03:10:29 +0700

    Добавлены не реализованные прототипы трех функции в классе статистики

--- lab1_ootrpo/Statistics.cpp
+++ lab1_ootrpo/Statistics.cpp
@@ -18,12 +18,24 @@ double Statistics::SumElements(vector<double> elements)
     return sum;
 }
 
-double Statistics::SumElementsAbs(vector<double> elements)
+double Statistics::MeanElements(vector<double> elements)
 {
-    double sum = 0.0;
-    for_each(elements.begin(), elements.end(), [&](double elem)
-    {
-        sum += abs(elem);
-    });
-    return sum;
+    double mean = 0.0;
+    mean = this->SumElements(elements) / (double) elements.size();
+    return mean;
+}
+
+double Statistics::GetExpectedValue(vector<double> elements)
+{
+
+}
+
+double Statistics::GetDispersion(vector<double> elements)
+{
+
+}
+
+double Statistics::GetMedian(vector<double> elements)
+{
+
 }
--- lab1_ootrpo/Statistics.h
+++ lab1_ootrpo/Statistics.h
@@ -12,7 +12,10 @@ class Statistics
 	~Statistics(); // деструктор по умолчанию
 
 	double SumElements(std::vector<double> elements);
-	double SumElementsAbs(vector<double> elements);
+	double MeanElements(vector<double> elements);
+	double GetExpectedValue(vector<double> elements);
+	double GetDispersion(vector<double> elements);
+	double GetMedian(vector<double> elements);
 
 	private:
 };
